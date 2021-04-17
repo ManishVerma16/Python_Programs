390ba169d949013473f0a1ec487166b81c6b43d1
Manish Verma <vermam318@gmail.com>
2021-04-17 23:59:36 +0530

    motion added

--- car_game/main.py
+++ car_game/main.py
@@ -15,12 +15,19 @@ def car(x, y):
 def game_loop():
     x = (display_width*0.45)
     y = (display_height*0.8)
+    x_change = 0
     bumped=False
     while not bumped:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 bumped = True
         
+        if event.type==pygame.KEYDOWN:
+            if event.type == pygame.K_LEFT:
+                x_change = -5
+            if event.type == pygame.K_RIGH:
+                x_change = 5
+
         gameDisplays.fill(gray)
         car(x, y)
         pygame.display.update()
